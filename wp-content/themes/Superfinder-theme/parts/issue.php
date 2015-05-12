87163d729899c6db2767f3e1acf5fc3276028744
Guillaume <horber.guillaume@orange.fr>
2015-05-12 15:25:24 +0200

    Modification Design Issue

--- wp-content/themes/Superfinder-theme/parts/issue.php
+++ wp-content/themes/Superfinder-theme/parts/issue.php
@@ -1,18 +1,23 @@
 <?php if ($res) : ?>
-	<div id="listing-issues" class="row">
-		<ul class="small-block-grid-1">
+<!--<div id="coverissue"></div> -->
+<h4 class="h4issueinfo">Issue Information</h4>
+	<div id="listing-issues" class="row"> <!-- GREEN -->
+	
+		<ul>
 			<?php foreach($res as $r) : ?>
-			<li class="issue row">
-				<img class="issue_cover columns small-6" src="<?php echo $r->image->small_url; ?>" alt="<?php echo $r->name; ?>">
-				
-				<h3>Nom : <a href="?issue=<?php echo $r->id; ?>&amp;issuename=<?php echo $r->name; ?>">
-							<?php echo $r->name; ?>
-						</a></h3>
-				<small><?php echo $r->cover_date; ?></small>
-			</li>
+			<div class="large-4 columns" id="descriptionissue">
+				<h2 class="h2issue">Name : <?php echo $r->name; ?></h3>
+				<h4 class="h4issue">Volume : <?php echo $r->volume->name; ?></h4>
+				<h4 class="h4issue">Issue number : <?php echo $r->issue_number; ?></h>
+				<h4 class="h4issue">Cover Date : <?php echo $r->cover_date; ?></h4>
+			</div>
+			<div class="large-8 columns" id="issue">
+				<a href="?issue=<?php echo $r->id; ?>">					
+					<img class="issue_cover" src="<?php echo $r->image->small_url; ?>" alt="<?php echo $r->name; ?>">
+				</a>
+			</div>
+			
 		<?php endforeach; ?>
 		</ul>
-		<a href="#" class="button">Ajouter à ma collection</a>
-		<a href="#" class="button">Ajouter à ma wishlist</a>
 	</div>
 <?php endif; ?>
