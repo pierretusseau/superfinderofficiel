c13366fb5bb3c2983066252b2cf4177ad22ec434
Pierre Tusseau <pierre.tusseau@gmail.com>
2015-05-12 15:27:37 +0200

    Revert d0c9bce..87163d7
    This rolls back to commit d0c9bce2ce048d91e0e7099ae6d3319ceaed1e30.

--- wp-content/themes/Superfinder-theme/index.php
+++ wp-content/themes/Superfinder-theme/index.php
@@ -1,10 +1,8 @@
 <?php get_header(); ?>
+<div class="row">
+	<div class="small-12 large-8 columns" role="main">
 
-<<<<<<< Updated upstream
 <div id="main" class="row">
-=======
-	<div id="main" class="row">
->>>>>>> Stashed changes
 		<a href="index.php"><h1>Superfinder</h1></a>
 
 		<?php // include_once('parts/prototype/user_link.php'); ?>
@@ -37,7 +35,34 @@
 		?>
 
 	</div> <!-- FIN DE LA ROW PRINCIPALE -->
-
 	
-<?php  include_once('parts/structure/dashboard.php'); ?>
+	<?php if ( have_posts() ) : ?>
+
+		<?php do_action('foundationPress_before_content'); ?>
+
+		<?php while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part( 'content', get_post_format() ); ?>
+		<?php endwhile; ?>
+
+		<?php else : ?>
+			<?php get_template_part( 'content', 'none' ); ?>
+
+		<?php do_action('foundationPress_before_pagination'); ?>
+
+	<?php endif;?>
+
+
+
+	<?php if ( function_exists('FoundationPress_pagination') ) { FoundationPress_pagination(); } else if ( is_paged() ) { ?>
+		<nav id="post-nav">
+			<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'FoundationPress' ) ); ?></div>
+			<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'FoundationPress' ) ); ?></div>
+		</nav>
+	<?php } ?>
+
+	<?php do_action('foundationPress_after_content'); ?>
+
+	</div>
+	<?php get_sidebar(); ?>
+</div>
 <?php get_footer(); ?>
