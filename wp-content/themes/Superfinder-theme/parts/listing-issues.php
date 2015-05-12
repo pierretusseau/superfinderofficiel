037d3f582326a50aa1a79bd9cb3c945178387f30
flob67 <flob-67@hotmail.fr>
2015-05-12 15:50:07 +0200

    list issues
    listing des issues

--- wp-content/themes/Superfinder-theme/parts/listing-issues.php
+++ wp-content/themes/Superfinder-theme/parts/listing-issues.php
@@ -2,12 +2,12 @@
 	<a href="index.php"> - Retourner en arrière</a>
 	<h2><?php echo $_GET['volumename']; ?></h2>
 	<div id="listing-issues" class="row">
-		<ul class="small-block-grid-4">
+		<ul class="small-block-grid-5">
 			<?php foreach($res as $r) : ?>
 			<li class="issue">
 				<a href="?issue=<?php echo $r->id; ?>">
+                    <div class="container"><img class="issue_cover" src="<?php echo $r->image->small_url; ?>" alt="<?php echo $r->name; ?>"></div>
 					<h2>Issue ... #<?php echo $r->issue_number; ?></h2>
-					<img class="issue_cover" src="<?php echo $r->image->small_url; ?>" alt="<?php echo $r->name; ?>">
 					<h3><?php echo $r->name; ?></h3>
 				</a>
 				<small><?php echo $r->cover_date; ?></small>
